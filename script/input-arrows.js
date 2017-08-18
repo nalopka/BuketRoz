171c257a65e908e0aca9c5f32326bbb0000b9dd4
Nalopka <Nalopka@gmail.com>
2017-08-18 17:02:19 +0700

    update count jq

--- script/input-arrows.js
+++ script/input-arrows.js
@@ -1,33 +1,46 @@
 $(document).ready(function(){
 	var	arrowUp = $(".arrowup");
 	var arrowDown = $(".arrowdown");
-	var numberValue = $("#rose-count").attr("value");
-	var totalPrice;
-	var salePrice;
+	var numberValue = $("#rose-count").val();
+	var totalPrice, salePrice, currentCount;
 	var startTotalPrice = numberValue * 80;
 	var startSalePrice = numberValue * 72;
 	$("#total-cost").text(startTotalPrice + " P");
 	$("#sale-cost").text(startSalePrice + " ");
-	arrowUp.on("click", function(){
-		numberValue++;
-		$("#rose-count").attr("value", numberValue);
+
+	$("#rose-count").on("change", function(){
+		numberValue = $("#rose-count").val();
+		console.log(numberValue);
+
+		$("#rose-count").val(numberValue);
 		totalPrice = numberValue * 80;
 		salePrice = numberValue * 72;
-		console.log(salePrice);
+		$("#total-cost").text(totalPrice + " P");
+		$("#sale-cost").text(salePrice + " ");
+		
+	});
+	
+
+
+	arrowUp.on("click", function(){
+		numberValue++;
+		$("#rose-count").val(numberValue);
+		totalPrice = numberValue * 80;
+		salePrice = numberValue * 72;
 		$("#total-cost").text(totalPrice + " P");
 		$("#sale-cost").text(salePrice + " ");
 
 	});
 	arrowDown.on("click", function(){
 		numberValue--;
-		$("#rose-count").attr("value", numberValue);
-		totalPrice = numberValue * 80;
-		salePrice = numberValue * 72;
-		$("#total-cost").text(totalPrice + " P");
-		$("#sale-cost").text(salePrice + " ");
-
+		if (numberValue >= 2) {
+			$("#rose-count").val(numberValue);
+			totalPrice = numberValue * 80;
+			salePrice = numberValue * 72;
+			$("#total-cost").text(totalPrice + " P");
+			$("#sale-cost").text(salePrice + " ");
+		} else {
+			numberValue = 2;}
 	});
 	
-
-
 });
