6e769bb271fb8f5b00bbc59d42f74049d708f958
Nalopka <Nalopka@gmail.com>
2017-08-21 16:57:56 +0700

    slider in progress

---
+++ script/more-photos.js
@@ -0,0 +1,12 @@
+$(function(){
+	var firstRose = $(".first-rose");
+	var secondRose = $(".second-rose");
+	var morePhotos = $(".more-rose");
+	var randomNumber, secondNumber;
+	morePhotos.on("click", function(){
+		randomNumber = 1 + Math.floor(Math.random() * 9);
+		secondNumber = randomNumber + 1;
+		firstRose.css("background-image", " url('img/our-rose/rose"+ randomNumber +".png') ");
+		secondRose.css("background-image", " url('img/our-rose/rose"+ secondNumber  +".png') ")
+	});
+});
