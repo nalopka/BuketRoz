d0ce390a79d07cfd5f99593f6c3cf689851dc2d1
Nalopka <Nalopka@gmail.com>
2017-08-28 17:01:40 +0700

    wrk

--- script/more-photos.js
+++ script/more-photos.js
@@ -10,12 +10,22 @@ $(function(){
 
 
 	moreImages.on("click", function(){
+
+		bouquet.removeClass("zoom-photo").css({
+			"opacity":"1",
+			"visibility": "visible"});
+
 		moreImages.css("display", "none");
 		$("body").attr("data-popup", "");
 	});
 	bouquet.on("click",function(e){
 		e.stopPropagation();
-		moreImages.toggleClass("zoom-photo");
+		bouquet.css({  
+			"opacity":"0",
+			"visibility":"hidden",});
+		$(this).css({
+			"opacity":"1",
+			"visibility": "visible"}).toggleClass("zoom-photo");
 		});
 	});
 
