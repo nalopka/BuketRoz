0f77b7f2c1276aeb20b937b15b1decd3a8c1ce24
Nalopka <Nalopka@gmail.com>
2017-08-29 13:31:58 +0700

    wrk gallery

--- script/more-photos.js
+++ script/more-photos.js
@@ -3,31 +3,40 @@ $(function(){
 	var morePhotos = $(".more-rose");
 	var moreImages = $(".more-images");
 	var bouquet = $(".bouquet");
+	var currentbouquet;
 
 	morePhotos.on("click", function(){
 		moreImages.css("display", "flex");
 		$("body").attr("data-popup", "popup");
 
-
-	moreImages.on("click", function(){
-
-		bouquet.removeClass("zoom-photo").css({
-			"opacity":"1",
-			"visibility": "visible"});
-
-		moreImages.css("display", "none");
-		$("body").attr("data-popup", "");
-	});
-	bouquet.on("click",function(e){
-		e.stopPropagation();
-		bouquet.css({  
-			"opacity":"0",
-			"visibility":"hidden",});
-		$(this).css({
-			"opacity":"1",
-			"visibility": "visible"}).toggleClass("zoom-photo");
+		// -----
+		moreImages.on("click", function(){
+			moreImages.css("display", "none");
+			$("body").attr("data-popup", "");
+		});
+		// ------
+		bouquet.on("click",function(e){
+			e.stopPropagation();
+			currentbouquet = $(this).attr("data-item");
+			bouquet.css("opacity", "0");
+			$(".zoom").addClass("bouquet zoom-active").attr("data-item", currentbouquet );
 		});
 	});
+
+	$(".zoom").on("click", function(e){
+			e.stopPropagation();
+			$(this).removeClass("bouquet zoom-active");
+			bouquet.css("opacity", "1");
+			
+		});
+	$(".right").on("click", function(e){
+			e.stopPropagation();
+					$(".zoom").attr("data-item", currentbouquet++ );
+			});
+	$(".left").on("click", function(e){
+			e.stopPropagation();
+					$(".zoom").attr("data-item", currentbouquet-- );
+			});
 
 
 });
